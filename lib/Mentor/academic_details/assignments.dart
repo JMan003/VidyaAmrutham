3ee10a9921ee84da95bf256915f45c0d89a1ea65
Vishnu V P <vishnuvayalilepurayil@gmail.com>
2024-05-06 01:27:23 +0530

    add mentor assignments

--- lib/Mentor/academic_details/assignments.dart
+++ lib/Mentor/academic_details/assignments.dart
@@ -1,47 +1,62 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:vidyaamrutham/Teacher/controls/Assignment/AddAssignment.dart';
 
 class Assignments extends StatefulWidget {
-  const Assignments({Key? key}) : super(key: key);
+  const Assignments({super.key});
 
   @override
-  State<Assignments> createState() => _AssignmentsState();
+  _AssignmentsState createState() => _AssignmentsState();
 }
 
 class _AssignmentsState extends State<Assignments> {
+  Future listAssignments() async {
+    String? url = dotenv.env['SERVER'];
+
+    var response = await http.get(Uri.parse('https://$url/assignments'));
+    return jsonDecode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Assignments'),
       ),
-      body: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.album),
-              title: Text(''),
-              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('OPEN'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('MARK AS DONE'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-              ],
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+        future: listAssignments(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  for (var assignment in snapshot.data["result"])
+                    Card(
+                      child: ListTile(
+                        title: ElevatedButton(
+                          child: Text(
+                              "${assignment['title']} - ${assignment['class']} ${assignment['division']}"),
+                          onPressed: (){
+                            
+                          },
+                        ),
+                        subtitle: Text(assignment['description']),
+                        trailing: Text(
+                            assignment['deadline'].toString().substring(0, 10)),
+                      ),
+                    ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
 }
-
